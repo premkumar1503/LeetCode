b4f33eef29827fcbb8f10183819b5273914102a8
premkumar1503 <premkumar15104@gmail.com>
2025-04-05 11:14:29 +0530

    Leetcode Problem

---
+++ src/_1863_Sum_of_All_Subset_XOR_Totals_.java
@@ -0,0 +1,32 @@
+class Solution
+{
+    public int subsetXORSum(int[] nums)
+    {
+        return dfs(nums, 0, 0);
+    }
+
+    private int dfs(int[] nums, int index, int currentXor)
+    {
+        if (index == nums.length)
+        {
+            return currentXor;
+        }
+
+        int withCurrent = dfs(nums, index + 1, currentXor ^ nums[index]);
+
+        int withoutCurrent = dfs(nums, index + 1, currentXor);
+
+        return withCurrent + withoutCurrent;
+    }
+}
+
+public class _1863_Sum_of_All_Subset_XOR_Totals_
+{
+    public static void main(String[] args)
+    {
+        int[] nums = {1, 3, 6 ,8, 10};
+        Solution solution = new Solution();
+        int result = solution.subsetXORSum(nums);
+        System.out.println("Sum of all subset XOR totals: " + result);
+    }
+}
